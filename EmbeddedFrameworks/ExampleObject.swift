53d16b1873164f9307c85b89c4bc7100519670b7
Justin Makaila <justinmakaila@gmail.com>
2014-12-02 17:16:14 -0500

    Removed ReactiveCocoa

---
+++ EmbeddedFrameworks/ExampleObject.swift
@@ -0,0 +1,9 @@
+//
+//  ExampleObject.swift
+//  EmbeddedFrameworks
+//
+//  Created by Justin Makaila on 12/2/14.
+//  Copyright (c) 2014 Justin Makaila. All rights reserved.
+//
+
+import Foundation
